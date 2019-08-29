a23a2e4a7ac118b445b93d2148af472c3977d09e
VPN Shelley <shell@CoreyA.(none)>
2019-08-29 10:14:19 -0500

    no special characters

--- Bangazon/Models/Product.cs
+++ Bangazon/Models/Product.cs
@@ -18,10 +18,14 @@ namespace Bangazon.Models
 
         [Required]
         [StringLength(255)]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$",
+         ErrorMessage = "Special Characters are not allowed.")]
         public string Description { get; set; }
 
         [Required]
         [StringLength(55, ErrorMessage="Please shorten the product title to 55 characters")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$",
+         ErrorMessage = "Special Characters are not allowed.")]
         public string Title { get; set; }
 
         [Required]
